e6e5c89ef23695484f521ebe75a8dcd09a2ca4f7
btysingh337@mail.com <gsmotivation00007@gmail.com>
2024-04-28 12:04:27 +0530

    added

--- file_1.c
+++ file_1.c
@@ -7,7 +7,7 @@ int main() {
     scanf("%d %d", &number1, &number2);
 
     // calculate the sum
-    sum = number1 + number2;    
+        
     
     printf("%d + %d = %d", number1, number2, sum);
     return 0;
